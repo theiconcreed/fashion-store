c22ee3d5c8eabbb046f87f195ae573a857712801
theiconcreed <151725304+theiconcreed@users.noreply.github.com>
2025-02-13 05:13:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.filter__controls li').on('click', function () {
+            $('.filter__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.product__filter').length > 0) {
+            var containerEl = document.querySelector('.product__filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+})(jQuery);
